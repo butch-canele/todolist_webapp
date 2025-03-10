c131bc4daed68caa1fb90ed21831d1079df9fa7d
butch <canele.yabuuchi@gmail.com>
2025-03-10 18:11:34 +0900

    [fix] db.py FK制約を接続のたびに設定

--- db.py
+++ db.py
@@ -3,16 +3,23 @@ import sqlite3
 DATABASE = "todo.db"
 
 
-# todoテーブル、usersテーブルを作成する関数
-def create_tables():
-    con = sqlite3.connect(DATABASE)
+# DBへの接続を開始する関数
+def connect_db():
+    con=sqlite3.connect(DATABASE)
 
     # 外部キー制約を有効化
     con.execute("PRAGMA foreign_keys = ON;")
+    
+    return con
+
+
+# todoテーブル、usersテーブルを作成する関数
+def create_tables():
+    con = connect_db()
 
     cur = con.cursor()
     cur.execute("""CREATE TABLE IF NOT EXISTS users (
-        user_id TEXT,
+        user_id TEXT UNIQUE,
         user_name TEXT,
         password TEXT
         )""")
@@ -32,7 +39,7 @@ def create_tables():
 
 # すべてのToDoの読み込み
 def get_todos(u_id):
-    con = sqlite3.connect(DATABASE)
+    con = connect_db()
     cur = con.cursor()
     cur.execute("SELECT * FROM todo WHERE user_id=?", (u_id,))
     todos = cur.fetchall()
@@ -43,7 +50,7 @@ def get_todos(u_id):
 
 # ToDoの追加
 def add_todo(t_id, todo, u_id):
-    con = sqlite3.connect(DATABASE)
+    con = connect_db()
     cur = con.cursor()
     cur.execute("INSERT INTO todo (todo_id, todo, is_done, user_id) VALUES (?, ?, 0, ?)", (t_id, todo, u_id))
     con.commit()
@@ -53,7 +60,7 @@ def add_todo(t_id, todo, u_id):
 
 # ToDoの更新
 def update_todo(id):
-    con = sqlite3.connect(DATABASE)
+    con = connect_db()
     cur = con.cursor()
     cur.execute("UPDATE todo SET is_done=1 WHERE todo_id=?", (id,))
     con.commit()
@@ -63,7 +70,7 @@ def update_todo(id):
 
 # ToDoの削除
 def delete_todo(id):
-    con = sqlite3.connect(DATABASE)
+    con = connect_db()
     cur = con.cursor()
     cur.execute("DELETE FROM todo WHERE todo_id=?", (id,))
     con.commit()
@@ -73,7 +80,7 @@ def delete_todo(id):
 
 # ユーザーの追加
 def add_user(id, name, password):
-    con = sqlite3.connect(DATABASE)
+    con = connect_db()
     cur = con.cursor()
     cur.execute(
         "INSERT INTO users (user_id, user_name, password) VALUES (?, ?, ?)",
@@ -86,7 +93,7 @@ def add_user(id, name, password):
 
 # ユーザーネームでのユーザーの読み込み
 def get_user_by_name(name):
-    con = sqlite3.connect(DATABASE)
+    con = connect_db()
     cur = con.cursor()
     cur.execute("SELECT * FROM users WHERE user_name=?", (name,))
     user = cur.fetchall()
@@ -97,7 +104,7 @@ def get_user_by_name(name):
 
 # ユーザーIDでのユーザーの読み込み
 def get_user_by_id(id):
-    con = sqlite3.connect(DATABASE)
+    con = connect_db()
     cur = con.cursor()
     cur.execute("SELECT * FROM users WHERE user_id=?", (id,))
     user = cur.fetchall()
